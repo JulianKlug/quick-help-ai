681fb1b7d92bb3e24bc42bc0b88637622b361c98
monsieurwave <tensu.wave@gmail.com>
2020-09-05 02:12:04 +0200

    Working templated prompt

---
+++ backend/generate_prompt/matching.py
@@ -0,0 +1,18 @@
+import spacy
+nlp = spacy.load("de_core_news_sm")
+
+faq_file = '../templates/FAQ.txt'
+
+text_file = open(faq_file, "r")
+faq_items = text_file.read().split('\n\n')
+faq_items = list(map(nlp, faq_items))
+
+def sort_match(input):
+    input = nlp(input)
+    similarity_scores = [input.similarity(faq_item) for faq_item in faq_items]
+    sorted_faq_items = [x for _, x in sorted(zip(similarity_scores, faq_items), reverse=True)]
+    return sorted_faq_items
+
+
+
+sort_match('Netzwerk im Energiesparmodus')
--- backend/src/api.js
+++ backend/src/api.js
@@ -2,16 +2,20 @@ import axios from 'axios';
 import fs from 'fs';
 import auth from "../auth";
 
-const template = fs.readFileSync('./templates/FAQ.txt', 'utf8');
+const faq_template = fs.readFileSync('./templates/FAQ.txt', 'utf8');
+const intent = fs.readFileSync('./templates/intent_specification.txt', 'utf8');
+const good_behaviour = fs.readFileSync('./templates/good_behaviour_examples.txt', 'utf8');
 
 const url = 'https://api.openai.com/v1/engines/davinci/completions';
 
-const temp = "I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \\"
-Unknown\\".\\n\\nQ: What is human life expectancy in the United States?\\nA: Human life expectancy in the United States is 78 years.\\n\\nQ: Who was president of the United States in 1955?\\nA: Dwight D. Eisenhower was president of the United States in 1955.\\n\\nQ: Which party did he belong to?\\nA: He belonged to the Republican Party.\\n\\nQ: What is the square root of banana?\\nA: Unknown\\n\\nQ: How does a telescope work?\\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\\n\\nQ: Where were the 1992 Olympics held?\\nA: The 1992 Olympics were held in Barcelona, Spain.\\n\\nQ: How many squigs are in a bonk?\\nA: Unknown\\n\\nQ: Do monkeys like bananas?\\n\""
-
 const answer_question = async (input) => {
     const question = 'Q: ' + input;
-    const templated_prompt = template + '\n\n' + question + '\n'
+    const separator = '\n\n'
+    const ending = '\n'
+    const templated_prompt = intent + separator
+        + faq_template + separator
+        + good_behaviour + separator
+        + question + ending
     const data = {
       prompt: templated_prompt,
       temperature:0,
