61550cdfdfa39f33fa56ed3bc1e1072edd3c21c2
klugh <klugh@student.ethz.ch>
2020-09-05 14:07:35 +0200

    added more answer filtering

--- code/preprocess_data/prep_scraped_data.py
+++ code/preprocess_data/prep_scraped_data.py
@@ -2,6 +2,10 @@
 Script to convert the structured json data created by the scraper into a text_file
 """
 import json
+import re
+
+#todo remove "schliesse diskussion"
+#todo remove questions
 
 with open('../scraper/data/results/results.json') as json_file:
     data = json.load(json_file)
@@ -10,15 +14,27 @@ discussion_textfile = open("../backend/templates/discussion.txt", "w")
 
 
 def filter_relevant_QA(elem):
-    # filter out questions that have no answer and those started by moderator
-    if ('answer' in elem) and not (elem['startedByEmployee']):
+    # filter out questions that have no answer, those that have a question in the answer and those started by moderator
+    if ('answer' in elem) and not (elem['startedByEmployee']) and (len(elem['answer'].split('?')) == 1):
         return elem
+
+
+def clean_answer(answer):
+    if answer.startswith('Hallo'):
+        # remove unnecessary "hallo username" from answer
+        answer = answer.split('\n')[1:]
+        answer = '\n'.join(answer)
+    answer = answer.replace('\n', ' ')
+    answer = answer.replace('Christoph', 'QAI-bot')
+    # remove usernames (they start with @ most of the times)
+    answer = re.sub(r'@\d*\b', '', answer)
+    return answer
 
 
 for elem in data:
     if filter_relevant_QA(elem):
         question = elem['question'].replace('\n', ' ')
-        answer = elem['answer'].replace('\n', ' ')
+        answer = clean_answer(elem['answer'])
         question_line = 'Q: {}\n'.format(question)
         discussion_textfile.write(question_line)
         answer_line = 'A: {}'.format(answer)
